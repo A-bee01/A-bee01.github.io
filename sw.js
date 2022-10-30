1ca5844e979fbfb2559a3a46101081937efed4cd
tech-girl <abbydeew@gmail.com>
2022-10-30 21:52:18 +0100

    Fixed Service Worker Caching Issues

--- sw.js
+++ sw.js
@@ -1,20 +1,43 @@
-const cacheName = 'girls-nest';
+let cacheName = "girls-nest";
+let filesToCache = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/css/common-style.css",
+  "/css/bootstrap.css",
+  "/js/script.js",
+  "/images/icon.png",
+  "/images/logo.png",
+  "/images/Screenshot1.png",
+  "/images/Screenshot2.png",
+  "/images/Screenshot3.png",
+  "/images/Screenshot4.png",
+  "/images/unsplash_0cgpyigyIkM.jpg",
+  "/images/unsplash_jEEYZsaxbH4.jpg",
+  "/images/unsplash_Sn04BHfa2AY.jpg",
+  "/images/unsplash_vLZC-lONFg0.jpg",
+  "/get-started/index.html",
+  "/girls-nest/index.html",
+  "/girls-nest/audio.js",
+  "/girls-nest/resources/app-style.css",
+  "/girls-nest/resources/wom.css",
+  "/girls-nest/resources/wom.js",
+  "https://kit.fontawesome.com/864d3cb9a1.js",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
+];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(cacheName));
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
 });
 
-self.addEventListener('fetch', async (event) => {
-  if ((event.request.destination === 'images' || event.request.url.includes("/images/")) && event.request.url.includes("https://Tech-Girl1.github.io/")) {
-    event.respondWith(caches.open(cacheName).then((cache) => {
-      return cache.match(event.request).then((cachedResponse) => {
-        return cachedResponse || fetch(event.request.url).then((fetchedResponse) => {
-          cache.put(event.request, fetchedResponse.clone());
-          return fetchedResponse;
-        });
-      });
-    }));
-  } else {
-    return;
-  }
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
 });
